6efc3741d49f60b624a9a247b415f37df2b4311b
Alice Dee <alice.dee@guardian.co.uk>
2020-02-05 02:28:33 +0000

    Improved card style and multiple neon headings

--- assets/js/neon.js
+++ assets/js/neon.js
@@ -1,69 +1,80 @@
-let textHolderAl = document.getElementsByClassName("neon-text-container")[0];
-let textAl = textHolderAl.innerText;
-console.log(textAl);
-let charsAl = textAl.length;
-console.log(charsAl);
-let newTextAl = "";
-var i;
+let allTextHolders = document.getElementsByClassName("neon-text-container"); 
+console.log(allTextHolders);
+let numTextHolders = allTextHolders.length;
+var n;
 
-for (i = 0; i < charsAl; i+=1) {
-  newTextAl += "<span>" + textAl.charAt(i) + "</span>";
-}
+for (n = 0; n < numTextHolders; n+=1) {
+  let textHolderAl = allTextHolders[n];
 
-textHolderAl.innerHTML = newTextAl;
-console.log(textHolderAl);
+  // let textHolderAl = document.getElementsByClassName("neon-text-container")[0];
+  let textAl = textHolderAl.innerText;
+  let charsAl = textAl.length;
+  let newTextAl = "";
+  var i;
 
-let lettersAl = textHolderAl.getElementsByTagName("span");
-let flickersAl = [5, 7, 9, 11, 13, 15, 17];
-var randomLetterAl;
-var randomLetterNumAl;
-var flickerNumberAl;
-var counterAl;
+  for (i = 0; i < charsAl; i+=1) {
+    newTextAl += "<span>" + textAl.charAt(i) + "</span>";
+  }
+  textHolderAl.innerHTML = newTextAl;
+  console.log(textHolderAl);
+
+  let lettersAl = textHolderAl.getElementsByTagName("span");
+  let flickersAl = [15, 13, 9, 11, 13, 15, 17];
+  var randomLetterAl;
+  var randomLetterNumAl;
+  var flickerNumberAl;
+  var counterAl;
 
 
-function randomFromInterval(from,to) {
-  return Math.floor(Math.random()*(to-from+1)+from);
-}
-
-function hasClass(element, cls) {
-  return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-}
-
-function flickerAl() {
-  counterAl += 1;
-
-  if (counterAl === flickerNumberAl)
-  {
-    return;
+  function randomFromInterval(from,to) {
+    return Math.floor(Math.random()*(to-from+1)+from);
   }
 
-  setTimeout
-  (
-    function ()
-    {
-      if(hasClass(randomLetterAl, 'off')) {
-        randomLetterAl.className = '';
-      }
-      else {
-        randomLetterAl.className = 'off';
-      }
+  function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  }
 
+  function flickerAl() {
+    counterAl += 1;
+
+    if (counterAl === flickerNumberAl)
+    {
+      randomLetterAl.className = '';
+      // console.log(randomLetterAl);
+      // console.log(randomLetterAl.className);
+      return;
+    }
+
+    setTimeout
+    (
+      function ()
+      {
+        if(hasClass(randomLetterAl, 'off')) {
+          randomLetterAl.className = '';
+        }
+        else {
+          randomLetterAl.className = 'off';
+        }
+
+        flickerAl();
+      }, 30);
+  }
+
+  (function loop() {
+    var rand = randomFromInterval(1000,6000);
+
+    randomLetterNumAl = randomFromInterval(0, charsAl - 1);
+    randomLetterAl = lettersAl[randomLetterNumAl];
+
+    flickerNumberAl = randomFromInterval(0, 6);
+    flickerNumberAl = flickersAl[flickerNumberAl];
+
+    setTimeout(function() {
+      counterAl = 0;
       flickerAl();
-    }, 30);
+      loop();
+    }, rand);
+  }());
 }
 
-(function loop() {
-  var rand = randomFromInterval(500,3000);
 
-  randomLetterNumAl = randomFromInterval(0, charsAl - 1);
-  randomLetterAl = lettersAl[randomLetterNumAl];
-
-  flickerNumberAl = randomFromInterval(0, 6);
-  flickerNumberAl = flickersAl[flickerNumberAl];
-
-  setTimeout(function() {
-    counterAl = 0;
-    flickerAl();
-    loop();
-  }, rand);
-}());
